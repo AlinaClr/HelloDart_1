df055cad7d980841a67ef6251e8bd34cb50b20be
AlinaClr <1516690324@qq.com>
2022-05-14 12:22:23 +0000

    新增

--- 03_dcat.dart
+++ 03_dcat.dart
@@ -1,10 +1,10 @@
 /*
- * @Author: your name
- * @Date: 2022-04-21 08:09:54
- * @LastEditTime: 2022-04-21 08:45:14
- * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Author: AlinaClr 1516690324@qq.com
+ * @Date: 2022-05-14 10:11:44
+ * @LastEditors: AlinaClr 1516690324@qq.com
+ * @LastEditTime: 2022-05-14 10:33:12
  * @FilePath: /HelloDart_1/03_dcat.dart
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import 'dart:convert';
 import 'dart:io';
@@ -13,16 +13,56 @@ import 'package:args/args.dart';
 
 const lineNumber = 'line-number';
 
-void main(List<String> args){
+void main(List<String> args) {
   exitCode = 0;
+  final parser = ArgParser()..addFlag(lineNumber, negatable: false, abbr: 'n');
 
-  final parser = ArgParser();
-  parser.addFlag(lineNumber,negetable: false, abbr: 'n');
+  ArgResults argResults = parser.parse(args);
 
-  ArgParser argResults = parser.parse(args);
-  final paths = argResults.rest;
+  final paths = argResults.rest; // 剩余的命令行参数
 
-  dcat(paths,showLineNumbers: argResults[lineNumber] as bool);
-  
-  print('done');
+  dcat(paths, showLineNumbers: argResults[lineNumber] as bool);
+}
+
+Future<void> dcat(List<String> paths, {bool showLineNumbers = false}) async {
+  if (paths.isEmpty) {
+    print('type exit to quit.');
+    while (true) {
+      stdout.write(
+          '> '); // 这样就不换行了 
+      String? line = stdin.readLineSync();
+      print('${line}\n');
+
+      if (line?.toLowerCase() == 'exit') {
+        print('bye.');
+        break;
+      }
+    }
+  } else {
+    for (final path in paths) {
+      var lineNumber = 1;
+      final lines = utf8.decoder
+          .bind(File(path).openRead())
+          .transform(const LineSplitter());
+
+      try {
+        await for (final line in lines) {
+          if (showLineNumbers) {
+            stdout.write('${lineNumber++} ');
+          }
+          stdout.writeln(line);
+        }
+      } catch (_) {
+        await _handleError(path);
+      }
+    }
+  }
+}
+
+Future<void> _handleError(String path) async {
+  if (await FileSystemEntity.isDirectory(path)) {
+    stderr.writeln('Error: $path is a directory.');
+  } else {
+    exitCode = 2;
+  }
 }
